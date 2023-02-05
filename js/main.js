162eed76a9038e56106e5dfa2282609f825a8d27
Gunblade <mohamed.dev.egy@gmail.com>
2023-02-05 05:06:56 +0200

    added background change toggle with local storage

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const gearIcon = document.querySelector(`.gear`);
 const settingsBox = document.querySelector(`.settings_box`);
 const colorsLis = document.querySelectorAll(`.settings_options .colors li`);
 const landing = document.querySelector(`.landing`);
+let currBackground = 1;
+
 //check for local storage values
 
 let mainColor = localStorage.getItem(`main_color`);
@@ -30,7 +32,7 @@ gearIcon.addEventListener(`click`, function () {
 // switch colors
 
 colorsLis.forEach((li) => {
-  li.addEventListener(`click`, function (e) {
+  li.addEventListener(`click`, function () {
     // adds active class on selected color
     colorsLis.forEach((li) => {
       li.classList.remove(`active`);
@@ -47,15 +49,68 @@ colorsLis.forEach((li) => {
 });
 
 // auto change background
-
+let randomizeBack;
 let imagesCount = 5;
-let currBackground = 1;
-setInterval(() => {
-  if (currBackground === imagesCount) {
-    currBackground = 0;
+let randomBackground = true;
+function randomizeBackground() {
+  if (randomBackground === true) {
+    randomizeBack = setInterval(() => {
+      if (currBackground > imagesCount) {
+        currBackground = 1;
+      }
+      landing.style.cssText = `background-image: url(../assets/${currBackground}.jpg)`;
+      localStorage.setItem(`current_background`, currBackground);
+      currBackground++;
+    }, 7000);
   }
-  landing.style.cssText = `background-image: url(../assets/${
-    currBackground + 1
-  }.jpg)`;
-  currBackground++;
-}, 7000);
+}
+// set the background from local storage
+let currentBackgroundLocal = localStorage.getItem(`current_background`);
+
+if (currentBackgroundLocal !== null) {
+  landing.style.cssText = `background-image: url(../assets/${currentBackgroundLocal}.jpg)`;
+  currBackground = currentBackgroundLocal;
+}
+
+// background change toggle
+
+const randomBackEl = document.querySelectorAll(`.random_background span`);
+let checkOnLocalForBackgroundState = localStorage.getItem(`randomBackground`);
+
+if (checkOnLocalForBackgroundState !== null) {
+  if (checkOnLocalForBackgroundState === "true") {
+    randomBackground = true;
+  } else {
+    randomBackground = false;
+  }
+  // set active class on background buttons
+  randomBackEl.forEach((el) => {
+    el.classList.remove(`active`);
+  });
+  if (randomBackground === true) {
+    document.querySelector(`.random_background .yes`).classList.add(`active`);
+  } else {
+    document.querySelector(`.random_background .no`).classList.add(`active`);
+  }
+}
+
+randomizeBackground();
+
+randomBackEl.forEach((el) => {
+  el.addEventListener(`click`, () => {
+    randomBackEl.forEach((el) => {
+      el.classList.remove(`active`);
+    });
+
+    el.classList.add(`active`);
+    if (el.classList.contains(`yes`)) {
+      randomBackground = true;
+      localStorage.setItem(`randomBackground`, randomBackground);
+      randomizeBackground();
+    } else {
+      randomBackground = false;
+      localStorage.setItem(`randomBackground`, randomBackground);
+      clearInterval(randomizeBack);
+    }
+  });
+});
